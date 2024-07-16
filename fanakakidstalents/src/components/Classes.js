36efa8b0c48bc31db912e8fa4555175fee16cf1f
tobias <tobias.omondi@student.moringaschool.com>
2024-07-16 18:20:11 +0300

    portfolio

--- fanakakidstalents/src/components/Classes.js
+++ fanakakidstalents/src/components/Classes.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Asset1 from '../Asset/dance.jpg';
 import Asset2 from '../Asset/piano.png';
 import Asset3 from '../Asset/violin.jpg';
+import './Classes.css'
 
 const Classes = () => {
   const [showMoreBallet, setShowMoreBallet] = useState(false);
@@ -9,8 +10,8 @@ const Classes = () => {
   const [showMoreViolin, setShowMoreViolin] = useState(false);
 
   // Storing data to be shown on client side
-  const balletSummary = "Ballet is a graceful and elegant dance form characterized by precise movements and pointe shoes for female dancers. It requires years of rigorous training and discipline to master its intricate techniques and expressive artistry.";
-  const balletFullText = balletSummary + " Ballet is a powerful form of artistic expression that combines athleticism with grace, telling stories and evoking emotions through movement.";
+  const balletSummary = "FanakaKids Talent Academy offers exceptional ballet training for young dancers, focusing on grace, discipline, and technical proficiency.";
+  const balletFullText = balletSummary + "Our experienced instructors nurture budding talents and instill a lifelong love for this elegant art form.";
 
   const pianoSummary = "FanakaKids Talent Academy offers comprehensive piano instruction tailored to different age groups and skill levels. Our dedicated piano teachers focus on ";
   const pianoFullText = pianoSummary + "developing strong musical foundations, reading skills, and technical abilities while encouraging creativity and musical expression.";
@@ -19,11 +20,13 @@ const Classes = () => {
   const violinFullText = violinSummary + "Our supportive learning environment fosters a passion for the violin, allowing students to explore its beauty and versatility.";
 
   return (
-    <div className='menu-class'>
+    <div className='class-headings'>
       <h2>CLASSES</h2>
+      <div className='menu-class'>
 
       <div className='class-img'>
         <img src={Asset1} alt='Child dancing' />
+        <h3>Ballerina &  Danseur</h3>
         <p>{showMoreBallet ? balletFullText : balletSummary}</p>
         {showMoreBallet ? (
           <button onClick={() => setShowMoreBallet(false)}>READ LESS</button>
@@ -34,6 +37,7 @@ const Classes = () => {
 
       <div className='class-img'>
         <img src={Asset2} alt='a child playing piano' />
+        <h3>Pianist.</h3>
         <p>{showMorePiano ? pianoFullText : pianoSummary}</p>
         {showMorePiano ? (
           <button onClick={() => setShowMorePiano(false)}>READ LESS</button>
@@ -44,6 +48,7 @@ const Classes = () => {
 
       <div className='class-img'>
         <img src={Asset3} alt='a child playing violin' />
+        <h3>Violinist.</h3>
         <p>{showMoreViolin ? violinFullText : violinSummary}</p>
         {showMoreViolin ? (
           <button onClick={() => setShowMoreViolin(false)}>READ LESS</button>
@@ -51,6 +56,7 @@ const Classes = () => {
           <button onClick={() => setShowMoreViolin(true)}>READ MORE</button>
         )}
       </div>
+    </div>
     </div>
   );
 };
