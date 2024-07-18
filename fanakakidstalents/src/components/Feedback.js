2c4dbf46bcae185f62c50a4b8c7d51f191ccc606
tobias <tobias.omondi@student.moringaschool.com>
2024-07-18 12:08:17 +0300

    slider

--- fanakakidstalents/src/components/Feedback.js
+++ fanakakidstalents/src/components/Feedback.js
@@ -1,10 +1,11 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import './Feedback.css';
 import AvatarAsset from '../Asset/parents1.jpeg';
 import AvatarAsset1 from '../Asset/parents2.jpeg';
 import AvatarAsset2 from '../Asset/parents3.jpeg';
 import AvatarAsset3 from '../Asset/parents4.jpeg';
-import { IoIosArrowRoundBack , IoIosArrowRoundForward } from "react-icons/io";
+import { IoIosArrowRoundBack, IoIosArrowRoundForward } from 'react-icons/io';
 
 const feedbackData = [
   {
@@ -30,27 +31,42 @@ const feedbackData = [
 ];
 
 const Feedback = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? feedbackData.length - 2 : prevIndex - 1));
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === feedbackData.length - 2 ? 0 : prevIndex + 1));
+  };
+
   return (
     <div>
       <div className='Feedback-main'>
-        <h1>What Parents Say <br/> About Us</h1>
+        <h1>What Parents Say <br /> About Us</h1>
       </div>
       <div className='Slider'>
+        <IoIosArrowRoundBack className='arrow prev' onClick={prevSlide} />
         <div className='main-slider'>
           {feedbackData.map((item, index) => (
-            <div key={index}>
-              <img src={item.img} alt={`avatar of ${item.name}`} />
-              <h3>{item.name}</h3>
-              <p>{item.feedback}</p>
+            <div key={index} className={index === currentIndex ? 'slide active' : 'slide'}>
+              {index === currentIndex && (
+                <>
+                  <img src={item.img} alt={`avatar of ${item.name}`} />
+                  <h3>{item.name}</h3>
+                  <p>{item.feedback}</p>
+                </>
+              )}
             </div>
           ))}
         </div>
+        <IoIosArrowRoundForward className='arrow next' onClick={nextSlide} />
       </div>
 
-      <div className='btn-Gallery'>
-        <button type='submit'>Gallery</button>
-      </div>
-
+ <div className='btn-Gallery'>
+  <button type='submit'>Gallery</button>
+</div> 
     </div>
   );
 }
