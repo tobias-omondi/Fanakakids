b42fa8c9d84833763688d06f9b49ed2d830b1407
tobias <tobias.omondi@student.moringaschool.com>
2024-07-30 13:46:13 +0300

    gallery

--- fanakakidstalents/src/components/Media.js
+++ fanakakidstalents/src/components/Media.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import './Gallery.css'
 
 const Media = () => {
   return (
-    <div>
-      <h1>Gallery</h1>
+    <div className='gallery-container'>
+      <div className='Gallery-headings'>
+        <h1>  Gallery. </h1>
+        <h3>Amazing momemts. </h3>
+      </div>
+      <div></div>
     </div>
   )
 }
