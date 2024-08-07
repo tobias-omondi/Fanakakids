23d58e051931a34f8cafab4406599469c964db24
tobias <tobias.omondi@student.moringaschool.com>
2024-08-07 13:27:53 +0300

    Testing the website

--- fanakakidstalents/src/components/Classes.js
+++ fanakakidstalents/src/components/Classes.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import './Classes.css';
-import classAsset from '../Asset/classes.jpg'
-import Feedback  from './Feedback';
+import classAsset from '../Asset/classes.jpg';
+import Feedback from './Feedback';
 
 const Classes = () => {
   return (
     <>
-    <div className='container'>
-      <h1>Classes</h1>
-      <div className='main-container'>
-      <div className='paragrah-container'>
-        <p>FanakaKids Talent Academy is thrilled to offer exceptional ballet,
-            violin, and piano classes for young talents. Our dedicated instructors
-            nurture creativity, discipline, and a lifelong love for the arts.
-            Whether your child dreams of dancing gracefully, playing enchanting melodies, 
-            or captivating audiences with the violin, we provide a nurturing environment to 
-            help them shine. Join us and watch your child's artistic journey blossom!
-        </p>
+      <div className='container'>
+        <h1>Classes</h1>
+        <div className='main-container'>
+          <div className='paragrah-container'>
+            <p>
+              FanakaKids Talent Academy is thrilled to offer exceptional ballet,
+              violin, and piano classes for young talents. Our dedicated instructors
+              nurture creativity, discipline, and a lifelong love for the arts.
+              Whether your child dreams of dancing gracefully, playing enchanting melodies, 
+              or captivating audiences with the violin, we provide a nurturing environment to 
+              help them shine. Join us and watch your child's artistic journey blossom!
+            </p>
+            <img src={classAsset} alt='school children'/>
+          </div>
+        </div>
+        <Feedback />
       </div>
-      <div className='paragrah-image'>
-        <img src={classAsset} alt='school children'/>
-      </div>
-    </div>
-    </div>
-    <Feedback />
     </>
-  )
+  );
 }
 
 export default Classes;
